e54b88918428ca1fab0b952bdf0db6ec97e4dcd0
Shridhar <Shridhar.g.patil1@gmail.com>
2025-12-09 19:00:40 +0530

    uart node frame correction

--- mrt_ws/src/uart_node/uart_node/uart_node.py
+++ mrt_ws/src/uart_node/uart_node/uart_node.py
@@ -7,6 +7,7 @@ from all_interfaces.msg import DriveData, LinearBaseData, WristData, TelemetryDa
 from rclpy.qos import QoSProfile, ReliabilityPolicy
 import serial
 import struct
+import numpy as np
 
 NUM_ENCODERS = 6
 NUM_QUAD = 5 + 1 # 5 from timer, 1 through GPIO
@@ -31,7 +32,7 @@ class UARTBridge(Node):
 
 
         # Timer: to send UART frame -> 100 hz for 0.01 callback
-        #self.send_timer = self.create_timer(0.01, self.send_uart)
+        self.send_timer = self.create_timer(0.01, self.send_uart)
 
         # Timer: read incoming UART frame
         self.read_timer = self.create_timer(0.01, self.read_uart)
@@ -90,9 +91,20 @@ class UARTBridge(Node):
 
         telemetry_data = TelemetryData()
 
+        bytes_to_read = serial.inWaiting()
+
         total_len = (2 * NUM_ENCODERS) + (12 * NUM_QUAD) + (2 * NUM_ACS)
         try:
-            data = self.serial.read(total_len)
+            data = self.serial.read(bytes_to_read)
+
+            last_nl = data.rfind(b'\n')
+
+            print(last_nl)
+
+            data = data[last_nl - total_len:last_nl] #take only that slice frame
+
+            print("Raw frame: ", data)
+            print("Length of frame: ", len(data))
 
             if len(data) != total_len:
                 return   # incomplete frame
@@ -107,20 +119,23 @@ class UARTBridge(Node):
 
             for _ in range(NUM_QUAD):
                 position_values = data[i:i+4] 
-                position_value = (position_values[0] | position_values[1] << 8 | position_values[2] << 16 | position_values[3] << 24)
+                position_value = (position_values[3] | position_values[2] << 8 | position_values[1] << 16 | position_values[0] << 24)
                 position.append(position_value)
+                print(f"Position {_ + 1}: {position_value}")
                 i += 4
             
             for _ in range(NUM_QUAD):
                 speed_values = data[i:i+4] 
-                speed_value = (speed_values[0] | speed_values[1] << 8 | speed_values[2] << 16 | speed_values[3] << 24)
+                speed_value = (speed_values[3] | speed_values[2] << 8 | speed_values[1] << 16 | speed_values[0] << 24)
                 speed.append(speed_value)
+                print(f"Speed {_ + 1}: {speed_value}")
                 i += 4
             
             for _ in range(NUM_QUAD):
                 acceleration_values = data[i:i+4]
-                acceleration_value = (acceleration_values[0] | acceleration_values[1] << 8 | acceleration_values[2] << 16 | acceleration_values[3] << 24)
+                acceleration_value = (acceleration_values[3] | acceleration_values[2] << 8 | acceleration_values[1] << 16 | acceleration_values[0] << 24)
                 acceleration.append(acceleration_value)
+                print(f"Acceleration {_ + 1}: {acceleration_value}")
                 i += 4
 
             telemetry_data.position = position
@@ -130,20 +145,24 @@ class UARTBridge(Node):
             angle = []
 
             for _ in range(NUM_ENCODERS):
-                high_byte = data[i]
-                low_byte = data[i+1]
+                high_byte = np.uint16(data[i])
+                low_byte = np.uint16(data[i+1])
                 value = float((high_byte << 8) | low_byte)/100
+                print(f"Angle {_ + 1}: {value}")
                 angle.append(value)
                 i += 2
 
             telemetry_data.angle = angle
-            
+
+            print("Angle: ", angle)
+
             current = []
 
             for _ in range(NUM_ACS):
-                high_byte = data[i]
-                low_byte = data[i+1]
+                high_byte = np.uint16(data[i])
+                low_byte = np.uint16(data[i+1])
                 value = float((high_byte << 8) | low_byte)*0.01208791208 - 25 #scaling factor to convert from ADC 0-4095 to current value
+                print(f"Current {_ + 1}: {value}")
                 current.append(value)
                 i += 2
             
